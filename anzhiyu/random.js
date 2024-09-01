1fc23ff4ef34a321baae9ec704b10f47cf68dbfe
zizi143 <2950601037@qq.com>
2024-09-01 08:45:30 +0800

    Site updated: 2024-09-01 08:45:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/24/hello-world/"];function toRandomPost(){
+var posts=["2024/08/24/hello-world/","2024/09/01/拉康派精神分析 1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
