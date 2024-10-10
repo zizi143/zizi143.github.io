8bc51acc45e65870034c3694cebe628bccd2f6e4
zizi143 <2950601037@qq.com>
2024-10-10 18:35:49 +0800

    Site updated: 2024-10-10 18:35:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/24/hello-world/","2024/09/01/拉康派精神分析 1/"];function toRandomPost(){
+var posts=["2024/08/24/hello-world/","2024/09/01/拉康派精神分析 1/","2024/10/10/【实用主义】电子书资源 /"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
